7c36df444704e222c9877bb5e590d6cbb5f45cf5
Valentin Schabschneider <valentin.schabschneider@gmail.com>
2025-07-20 01:25:22 +0200

    Create dont-text-dont-call.js

---
+++ dont-text-dont-call.js
@@ -0,0 +1,15 @@
+/*
+  @title   dont text dont call - say now / dj_dave
+  @by      ValiShaby
+*/
+
+setcpm(120)
+
+$: note("[40 - - - 40 - - - 40 - - 40 - - 40 -]/4").sound("rolandtr808_bd").gain(0.3);
+$: note("[30 - - - 30 - - - 30 - - 30 - - 30 -]/4").sound("rolandtr808_bd").gain(2);
+$: note("[20 - - - 20 - - - 20 - - 20 - - 20 -]/4").sound("rolandtr808_bd").gain(1)._punchcard();
+
+$: note("[50 52 45 48]/32").dur(8).sound("sine")._punchcard();
+
+$: note("[72 71 71 71 72 74 74 74 74 74 72 72 72 71 71 71 72 74 76 76 76 76 72 72]/16").sound("tri").gain(0.7).decay(5).delay(0.5)._punchcard();
+$: note("[[72 71] 74 [72 71] 76]/16").sound("sine").gain(0.6)._punchcard();
