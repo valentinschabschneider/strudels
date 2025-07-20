71f3167e859af7fac1d4c4d52f005f3d76945342
Valentin Schabschneider <valentin.schabschneider@gmail.com>
2025-07-20 14:27:43 +0200

    Update dont-text-dont-call.js

--- dont-text-dont-call.js
+++ dont-text-dont-call.js
@@ -16,7 +16,7 @@ $: note(kickPattern).note(20).sound("rolandtr808_bd").gain(1)._punchcard();
 $: note("[50 52 45 48]/32").gain(.4).room(0.3).dur(8).sound("gm_drawbar_organ")._punchcard();
 
 // Main melody
-$: note("[72 71 71 71 72 74 74 74 74 74 72 72 72 71 71 71 72 74 76 76 76 76 72 72]/16").sound("gm_tremolo_strings").gain(0.6).room(1).decay(0.6).lpf(4000)._punchcard();
+$: note("[72 71 71 71 72 74 74 74 74 74 72 72 72 71 71 71 72 74 76 76 76 76 72 72]/16").late(1/32).sound("gm_tremolo_strings").gain(0.6).room(1).decay(0.6).lpf(4000)._punchcard();
 // Main melody support
 $: note("[[72 71] 74 [72 71] 76]/16").sound("gm_voice_oohs").gain(0.15).room(0.3)._punchcard();
 
