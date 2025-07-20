b3680496ba9280f71d511ae2563175d0280d8b2a
Valentin Schabschneider <valentin.schabschneider@gmail.com>
2025-07-20 14:10:41 +0200

    Update dont-text-dont-call.js

--- dont-text-dont-call.js
+++ dont-text-dont-call.js
@@ -6,13 +6,16 @@
 setcpm(120)
 
 $: note("[40 - - - 40 - - - 40 - - 40 - - 40 -]/4").sound("rolandtr808_bd").gain(0.3);
-$: note("[30 - - - 30 - - - 30 - - 30 - - 30 -]/4").sound("rolandtr808_bd").gain(2);
+$: note("[30 - - - 30 - - - 30 - - 30 - - 30 -]/4").sound("rolandtr808_bd").gain(1.5);
 $: note("[20 - - - 20 - - - 20 - - 20 - - 20 -]/4").sound("rolandtr808_bd").gain(1)._punchcard();
 
-$: note("[50 52 45 48]/32").dur(8).sound("sine")._punchcard();
+$: note("[50 52 45 48]/32").gain(.4).room(0.3).dur(8).sound("gm_drawbar_organ")._punchcard();
 
-$: note("[72 71 71 71 72 74 74 74 74 74 72 72 72 71 71 71 72 74 76 76 76 76 72 72]/16").sound("tri").gain(0.5).room(1).decay(0.6)._punchcard();
-$: note("[[72 71] 74 [72 71] 76]/16").sound("sine").gain(0.2).room(0.3)._punchcard();
+$: note("[72 71 71 71 72 74 74 74 74 74 72 72 72 71 71 71 72 74 76 76 76 76 72 72]/16")
+  //.sound("tri")
+  .sound("gm_tremolo_strings")
+  .gain(0.4).room(1).decay(0.6)._punchcard();
+$: note("[[72 71] 74 [72 71] 76]/16").sound("gm_voice_oohs").gain(0.2).room(0.3)._punchcard();
 
 $: sound("hh*4").pan("[.3 .43 .63 .7 .63 .43]/8").gain(sine.range(.05, .08).slow(2));
 
